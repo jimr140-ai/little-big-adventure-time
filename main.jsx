7f332186026bf40a5c0f3dfb5e510ef12649b601
jimr140-ai <jimr140@googlemail.com>
2026-02-28 19:34:59 +0000

    Update App import to use version 7

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './LittleBigAdventureTime_v6.jsx'
+import App from './LittleBigAdventureTime_v7.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
