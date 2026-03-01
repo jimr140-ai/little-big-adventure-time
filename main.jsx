90399bd81aa51f70fe8c2800fe68f02cd7fd0c63
jimr140-ai <jimr140@googlemail.com>
2026-03-01 11:29:56 +0000

    Update import to use LittleBigAdventureTime_v8

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './LittleBigAdventureTime_v7.jsx'
+import App from './LittleBigAdventureTime_v8.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
